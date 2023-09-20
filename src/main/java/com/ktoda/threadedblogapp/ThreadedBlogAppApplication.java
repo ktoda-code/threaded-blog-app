f823e54f712f6884ce8af8b1db79516d7d9d5928
Konstantin Todorov Andreev <ktod.andreev@gmail.com>
2023-09-20 11:57:19 +0300

    Initial commit
    Signed-off-by: Konstantin Todorov Andreev <ktod.andreev@gmail.com>

---
+++ src/main/java/com/ktoda/threadedblogapp/ThreadedBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.ktoda.threadedblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadedBlogAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadedBlogAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ktoda/threadedblogapp/ThreadedBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktoda.threadedblogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadedBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
